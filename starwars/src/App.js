fc40f0bb5643f61050d84d8c48cfcc208e117274
lyntechi <lynda-santiago@lambdastudents.com>
2020-06-12 13:02:08 -0400

    created props for function Character Component and its successfully passing data and displaying on screen

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-import Character from "./Character"
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -10,14 +10,15 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  
-  const [characters, setCharacters]=useState([])
+
+  const [characters, setCharacters] = useState([]);
+
   useEffect(() => {
     axios
       .get("https://swapi.py4e.com/api/starships/12/")
       .then((response) => {
         console.log("API responded successfully", response.data);
-        setCharacters(response.data)
+        setCharacters(response.data);
       })
       .catch((error) => {
         console.log("something went wrong with API response", error);
@@ -27,9 +28,10 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      
+        <Character characterName={characters.name} cargo={characters.cargo_capacity}/>;
+      
     </div>
-    
-    <Character />
   );
 };
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,23 +1,14 @@
 // Write your Character component here
-import React from 'react'
+import React from "react";
 
+function Character(props) {
+  return (
+    <div className="character-container">
+     <h1>Name: {props.characterName}</h1>
+     <h2>Cargo Capacity: {props.cargo}</h2>
 
-  function Character(props){
-
-
-return(
-
-<div className="character-container">
-
-
-</div>
-
-)
-
-
-
-
+    </div>
+  );
 }
-
 
 export default Character;
