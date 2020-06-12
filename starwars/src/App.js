b62558015cbb686991590bf88eea7b9541dcedbb
lyntechi <lynda-santiago@lambdastudents.com>
2020-06-12 13:59:52 -0400

    accidently had wrong api, switched it to correct api for characters & characters are successfully being displayed along with their birth years

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -4,21 +4,14 @@ import axios from "axios";
 import Character from "./components/Character";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
   const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     axios
-      .get("https://swapi.py4e.com/api/starships/12/")
+      .get("https://swapi.dev/api/people/")
       .then((response) => {
-        console.log("API responded successfully", response.data);
-        setCharacters(response.data);
+        console.log("API responded successfully", response.data.results);
+        setCharacters(response.data.results);
       })
       .catch((error) => {
         console.log("something went wrong with API response", error);
@@ -28,9 +21,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      
-        <Character characterName={characters.name} cargo={characters.cargo_capacity}/>;
-      
+      {characters.map((item, index) => {
+        return <Character key={index} characterName={item.name} birthYear={item.birth_year}/>;
+      })}
     </div>
   );
 };
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -4,9 +4,9 @@ import React from "react";
 function Character(props) {
   return (
     <div className="character-container">
-     <h1>Name: {props.characterName}</h1>
-     <h2>Cargo Capacity: {props.cargo}</h2>
-
+      <h1>Name: {props.characterName}</h1>
+      <h1>Birth Year: {props.birthYear}</h1>
+      {/* <h2>Cargo Capacity: {props.cargo}</h2> */}
     </div>
   );
 }
